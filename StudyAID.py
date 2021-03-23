a7cbb220cc8c910ac91547c591d017c0d1055930
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-23 17:25:48 +0100

    Add files via upload

---
+++ StudyAID.py
@@ -0,0 +1,19 @@
+print(' ')
+print(' |----------------------  testing ----------------------| ')
+print(' ')
+
+
+
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6, 7]
+y = [15, 22, 16, 12, 14, 15, 19]
+
+plt.plot(x, y)
+plt.plot(x, y, marker="*", markersize=2, color="green")
+plt.show()
+
+
+print(' ')
+print(' |----------------------  testing ----------------------| ')
+print(' ')
