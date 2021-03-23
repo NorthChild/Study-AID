6f82d600796cf61cf9d80528d24ee3d579d46ad1
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-23 17:27:29 +0100

    Update StudyAID.py

--- StudyAID.py
+++ StudyAID.py
@@ -1,8 +1,3 @@
-print(' ')
-print(' |----------------------  testing ----------------------| ')
-print(' ')
-
-
 
 import matplotlib.pyplot as plt
 
@@ -14,6 +9,101 @@ plt.plot(x, y, marker="*", markersize=2, color="green")
 plt.show()
 
 
-print(' ')
-print(' |----------------------  testing ----------------------| ')
-print(' ')
+
+
+
+
+
+
+x = np.linspace(0, 3 * np.pi, 500)
+y = np.sin(x)
+dydx = np.cos(0.5 * (x[:-1] + x[1:]))  # first derivative
+
+# Create a set of line segments so that we can color them individually
+# This creates the points as a N x 1 x 2 array so that we can stack points
+# together easily to get the segments. The segments array for line collection
+# needs to be (numlines) x (points per line) x 2 (for x and y)
+points = np.array([x, y]).T.reshape(-1, 1, 2)
+segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+fig, axs = plt.subplots(2, 1, sharex=True, sharey=True)
+
+# Create a continuous norm to map from data points to colors
+norm = plt.Normalize(dydx.min(), dydx.max())
+lc = LineCollection(segments, cmap='viridis', norm=norm)
+# Set the values used for colormapping
+lc.set_array(dydx)
+lc.set_linewidth(2)
+line = axs[0].add_collection(lc)
+fig.colorbar(line, ax=axs[0])
+
+# Use a boundary norm instead
+cmap = ListedColormap(['r', 'g', 'b'])
+norm = BoundaryNorm([-1, -0.5, 0.5, 1], cmap.N)
+lc = LineCollection(segments, cmap=cmap, norm=norm)
+lc.set_array(dydx)
+lc.set_linewidth(2)
+line = axs[1].add_collection(lc)
+fig.colorbar(line, ax=axs[1])
+
+axs[0].set_xlim(x.min(), x.max())
+axs[0].set_ylim(-1.1, 1.1)
+plt.show()
+
+
+
+
+
+
+from matplotlib.collections import EventCollection
+import numpy as np
+
+# Fixing random state for reproducibility
+# np.random.seed(19680801)
+
+# create random data
+xdata = np.random.random([90, 100])
+
+# split the data into two parts
+xdata1 = [xdata[0, :]]
+xdata2 = xdata[1, :]
+
+# sort the data so it makes clean curves
+xdata1.sort()
+xdata2.sort()
+
+# create some y data points
+ydata1 = xdata1 ** 2
+ydata2 = 1 - xdata2 ** 3
+
+# plot the data
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(xdata1, ydata1, color='tab:blue', label='Min Weely Study Hours')
+ax.plot(xdata2, ydata2, color='tab:orange', label='Reported Weekly Hours')
+
+# create the events marking the x data points
+xevents1 = EventCollection(xdata1, color='tab:blue', linelength=0.05)
+xevents2 = EventCollection(xdata2, color='tab:orange', linelength=0.05)
+
+# create the events marking the y data points
+yevents1 = EventCollection(ydata1, color='tab:blue', linelength=0.05,
+                           orientation='vertical')
+yevents2 = EventCollection(ydata2, color='tab:orange', linelength=0.05,
+                           orientation='vertical')
+
+# add the events to the axis
+ax.add_collection(xevents1)
+ax.add_collection(xevents2)
+ax.add_collection(yevents1)
+ax.add_collection(yevents2)
+
+# set the limits
+ax.set_xlim([0, 100])
+ax.set_ylim([0, 30])
+
+ax.set_title('Study Hours')
+
+# display the plot and legend
+ax.legend()
+plt.show()
